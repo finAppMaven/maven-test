5f14daa9e5aec59eb19ebc07fd186b93796831bf
ziejaCode <czarny25101@gmail.com>
2018-12-23 17:34:01 +0000

    Business Logic for app provided

--- gitHubRepoMavenTest/src/main/java/com/ziejacode/gitHubRepoMavenTest/App.java
+++ gitHubRepoMavenTest/src/main/java/com/ziejacode/gitHubRepoMavenTest/App.java
@@ -1,27 +1,42 @@
 package com.ziejacode.gitHubRepoMavenTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
-//import yourTax.UseTaxCalculator;
-
-
-
-
-/**
- * Hello world!
- *
- */
 public class App{
     
 	
-	
-	public static void main( String[] args ){
-//		UseTaxCalculator us = new UseTaxCalculator();
-//		
-//		List<Double> tax = us.calculateMyTax(543);
-//
-//    	System.out.println( "You sucessfuly complited this task " +  tax);
-    }
 
+	private static  double payeRate1 = 0.2;
+	private static  double payeRate2 = 0.4;
+	private static  double taxCreditAmount = 64.5;
 	
+	private  static double PRSIRate = 0.04;
+	
+	public static void main(String[] args) {
+
+		App us = new App();
+		
+		for(double d: us.calculateMyTax(543)) {
+			System.out.println("- " + d + " - ");
+		}		
+		System.out.println("Sorteds");
+	}
+	
+	
+	public List<Double> calculateMyTax(double wage) {
+		
+		List<Double> taxes = new ArrayList<Double>();
+		
+		double paye = wage * payeRate1 - taxCreditAmount;
+		taxes.add(paye);
+		
+		double prsi = wage * PRSIRate;
+		taxes.add(prsi);
+		
+		
+		return taxes;
+	}
+
+
 }
