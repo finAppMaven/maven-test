a09dbf3b0bf54875eea82b400ea1628e354e4240
ziejaCode <czarny25101@gmail.com>
2018-12-23 15:37:21 +0000

    init1

---
+++ gitHubRepoMavenTest/src/main/java/com/ziejacode/gitHubRepoMavenTest/App.java
@@ -0,0 +1,27 @@
+package com.ziejacode.gitHubRepoMavenTest;
+
+import java.util.List;
+
+//import yourTax.UseTaxCalculator;
+
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+    
+	
+	
+	public static void main( String[] args ){
+//		UseTaxCalculator us = new UseTaxCalculator();
+//		
+//		List<Double> tax = us.calculateMyTax(543);
+//
+//    	System.out.println( "You sucessfuly complited this task " +  tax);
+    }
+
+	
+}
